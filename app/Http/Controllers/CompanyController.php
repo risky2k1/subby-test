4cdb3ab174f6816674526fb6b9cb0783c7d546ef
Pham Minh Tuan <theloneranger241@gmail,com>
2023-03-22 17:05:45 +0700

    update company subscription 2

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Company;
+use Bpuig\Subby\Models\Plan;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
@@ -11,8 +13,36 @@ class CompanyController extends Controller
     {
         $companies = Company::query()->get();
 
-        return view('admin.master',[
-            'companies'=>$companies,
+        return view('admin.master', [
+            'companies' => $companies,
         ]);
-}
+    }
+
+    public function subscribeCompany(Request $request)
+    {
+        $company = Company::find($request->input('company_id'));
+
+        $plan = Plan::find($request->input('plan'));
+//    dd($company->subscription('main')->isActive());
+        // Subscribe the company to the selected plan
+//            if ($plan === '6-months') {
+////                dd('vao day');
+        if ($company->subscription()->isActive()) {
+            $company->subscription()->renew();
+        } else($company->newSubscription(
+            'main', // identifier tag of the subscription. If your application offers a single subscription, you might call this 'main' or 'primary'
+            $plan, // Plan or PlanCombination instance your subscriber is subscribing to
+            $plan->name, // Human-readable name for your subscription
+            $plan->description, // Description
+            Carbon::now(), // Start date for the subscription, defaults to now()
+            'free' // Payment method service defined in config
+        ));
+
+//                dd('plan sub succsses');
+//            } elseif ($plan === '12-months') {
+//                $company->newSubscription('main', '12-months')->create();
+//            }
+
+        return redirect()->route('admin.company.index')->with('success', 'Your subscription has been created.');
+    }
 }
